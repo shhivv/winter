2ae3c013bd216bbf3388621c138def44648c97cf
Shiv <shiv.7.shan@gmail.com>
2025-01-06 19:33:09 +0530

    define tokens

---
+++ token/token.go
@@ -0,0 +1,29 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL" // unknown character
+	EOF     = "EOF"
+
+	IDENT = "IDENT" // identifier
+	INT   = "INT"
+
+	ASSIGN = "="
+	PLUS   = "+"
+
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
