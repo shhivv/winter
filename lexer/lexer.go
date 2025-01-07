d6394e2f6ad398909eb9eff6be228b635dba52b4
Shiv <shiv.7.shan@gmail.com>
2025-01-07 07:48:37 +0530

    support keywords & idents

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -13,7 +13,7 @@ func (l *Lexer) readChar() {
 	if l.readPosition >= len(l.input) {
 		l.ch = 0 // ascii for NUL, indicates that either no tokens have been created, or EOF has been reached
 	} else {
-		l.ch = l.input[l.readPosition]
+		l.ch = l.input[l.readPosition] // string indexing produces ascii code (rune) instead of a string
 	}
 
 	l.position = l.readPosition
@@ -41,11 +41,32 @@ func (l *Lexer) NextToken() token.Token {
 	case '}':
 		tok = newToken(token.RBRACE, l.ch)
 	case 0:
-		tok.Literal = ""
+		tok.Literal = "" // string(0)
 		tok.Type = token.EOF
+  default:
+    if isLetter(l.ch){
+      tok.Literal = l.readIdentifier()
+      tok.Type  = token.LookupIdent(tok.Literal)
+      return tok // early return because readIdentifier already advances position
+    } else{
+      tok = newToken(token.ILLEGAL, l.ch)
+    }
 	}
 	l.readChar()
 	return tok
+}
+
+// reads the next word until whitespace (can be a keyword or ident)
+func (l *Lexer) readIdentifier() string{
+  position := l.position
+  for isLetter(l.ch){
+    l.readChar()
+  }
+
+  return l.input[position:l.position]
+}
+func isLetter(ch byte) bool {
+  return 'a' <= ch && ch >= 'z' || 'A' <= ch && ch >= 'Z' || ch == '_'
 }
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
