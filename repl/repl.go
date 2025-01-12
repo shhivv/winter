c863221c1739b1eeb31df2fd366803213f1bae69
Shiv <shiv.7.shan@gmail.com>
2025-01-12 17:38:53 +0530

    add statement parsing

--- repl/repl.go
+++ repl/repl.go
@@ -1,32 +1,32 @@
 package repl
 
 import (
-  "bufio"
-  "fmt"
-  "io"
-  "winter/lexer"
-  "winter/token"
+	"bufio"
+	"fmt"
+	"io"
+	"winter/lexer"
+	"winter/token"
 )
 
 const PROMPT = ">>> "
 
-func Start(in io.Reader, out io.Writer){
-  scanner := bufio.NewScanner(in)
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
 
-  for {
-    fmt.Print(PROMPT)
-    scanned := scanner.Scan()
+	for {
+		fmt.Print(PROMPT)
+		scanned := scanner.Scan()
 
-    if !scanned{
-      return
-    }
+		if !scanned {
+			return
+		}
 
-    line := scanner.Text()
+		line := scanner.Text()
 
-    l := lexer.New(line)
+		l := lexer.New(line)
 
-    for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken(){
-      fmt.Printf("%+v\n", tok)
-    }
-  }
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
 }
