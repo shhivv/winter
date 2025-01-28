22e6b3896ff3e970228c110fe925c17cdda3af3e
Shiv <shiv.7.shan@gmail.com>
2025-01-28 23:09:13 +0530

    parse prefix expressions

--- parser/parser.go
+++ parser/parser.go
@@ -43,9 +43,22 @@ func New(l *lexer.Lexer) *Parser {
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
 	p.registerPrefix(token.INT, p.parseIntegerLiteral)
+  p.registerPrefix(token.BANG, p.parsePrefixExpression)
+  p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 	return p
 }
 
+func (p *Parser) parsePrefixExpression() ast.Expression {
+  expression := &ast.PrefixExpression{
+    Token: p.curToken,
+    Operator: p.curToken.Literal,
+  }
+
+  p.nextToken()
+  expression.Right = p.parseExpression(PREFIX)
+
+  return expression
+}
 func (p *Parser) parseIdentifier() ast.Expression {
 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
 }
@@ -141,9 +154,14 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 	return stmt
 }
 
+func (p *Parser) noPrefixParseFnError(t token.TokenType) {
+  msg := fmt.Sprintf("no prefix parse function for %s found", t)
+  p.errors = append(p.errors, msg)
+}
 func (p *Parser) parseExpression(precedence int) ast.Expression {
 	prefix := p.prefixParseFns[p.curToken.Type]
 	if prefix == nil {
+    p.noPrefixParseFnError(p.curToken.Type)
 		return nil
 	}
 
