4dfbc77fd0b74257e432ba8199cd3b2e1843501f
Shiv <shiv.7.shan@gmail.com>
2025-01-29 11:27:58 +0530

    introduce precedences

--- parser/parser.go
+++ parser/parser.go
@@ -24,7 +24,19 @@ const (
 	CALL // foo(x)
 )
 
+var precedences = map[token.TokenType] int {
+  token.EQ: EQUALS,
+  token.NOT_EQ: EQUALS,
+  token.LT : LESSGREATER,
+  token.GT: LESSGREATER,
+  token.PLUS: SUM,
+  token.MINUS: SUM,
+  token.SLASH: PRODUCT,
+  token.ASTERISK: PRODUCT,
+}
+
 type Parser struct {
+
 	l         *lexer.Lexer
 	errors    []string
 	curToken  token.Token
@@ -43,21 +55,36 @@ func New(l *lexer.Lexer) *Parser {
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
 	p.registerPrefix(token.INT, p.parseIntegerLiteral)
-  p.registerPrefix(token.BANG, p.parsePrefixExpression)
-  p.registerPrefix(token.MINUS, p.parsePrefixExpression)
+	p.registerPrefix(token.BANG, p.parsePrefixExpression)
+	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 	return p
 }
 
-func (p *Parser) parsePrefixExpression() ast.Expression {
-  expression := &ast.PrefixExpression{
-    Token: p.curToken,
-    Operator: p.curToken.Literal,
+func (p *Parser) peekPrecedence() int {
+  if p, ok := precedences[p.peekToken.Type]; ok {
+    return p
   }
 
-  p.nextToken()
-  expression.Right = p.parseExpression(PREFIX)
+  return LOWEST
+}
 
-  return expression
+func (p *Parser) curPrecedence() int {
+  if p, ok := precedences[p.curToken.Type]; ok {
+    return p
+  }
+
+  return LOWEST
+}
+func (p *Parser) parsePrefixExpression() ast.Expression {
+	expression := &ast.PrefixExpression{
+		Token:    p.curToken,
+		Operator: p.curToken.Literal,
+	}
+
+	p.nextToken()
+	expression.Right = p.parseExpression(PREFIX)
+
+	return expression
 }
 func (p *Parser) parseIdentifier() ast.Expression {
 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
@@ -155,13 +182,13 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 }
 
 func (p *Parser) noPrefixParseFnError(t token.TokenType) {
-  msg := fmt.Sprintf("no prefix parse function for %s found", t)
-  p.errors = append(p.errors, msg)
+	msg := fmt.Sprintf("no prefix parse function for %s found", t)
+	p.errors = append(p.errors, msg)
 }
 func (p *Parser) parseExpression(precedence int) ast.Expression {
 	prefix := p.prefixParseFns[p.curToken.Type]
 	if prefix == nil {
-    p.noPrefixParseFnError(p.curToken.Type)
+		p.noPrefixParseFnError(p.curToken.Type)
 		return nil
 	}
 
