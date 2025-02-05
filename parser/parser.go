ec3226892c51bbfe81ebb655f1b394e2e8929e4b
Shiv <shiv.7.shan@gmail.com>
2025-02-05 22:47:51 +0530

    write some infix parsers

--- parser/parser.go
+++ parser/parser.go
@@ -57,9 +57,34 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 	p.registerPrefix(token.BANG, p.parsePrefixExpression)
 	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
+
+  p.infixParseFns = make(map[token.TokenType]infixParseFn)
+  p.registerInfix(token.PLUS, p.parseInfixExpression)
+  p.registerInfix(token.MINUS, p.parseInfixExpression)
+  p.registerInfix(token.SLASH, p.parseInfixExpression)
+  p.registerInfix(token.ASTERISK, p.parseInfixExpression)
+  p.registerInfix(token.EQ, p.parseInfixExpression)
+  p.registerInfix(token.NOT_EQ, p.parseInfixExpression)
+  p.registerInfix(token.LT, p.parseInfixExpression)
+  p.registerInfix(token.GT, p.parseInfixExpression)
 	return p
 }
 
+
+func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression { 
+
+  expression := &ast.InfixExpression{
+    Token: p.curToken,
+    Operator: p.curToken.Literal,
+    Left: left,
+  }
+
+
+  precedence := p.curPrecedence()
+  p.nextToken()
+  expression.Right = p.parseExpression(precedence)
+  return expression
+}
 func (p *Parser) peekPrecedence() int {
   if p, ok := precedences[p.peekToken.Type]; ok {
     return p
